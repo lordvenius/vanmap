85421264cad224f6036e4cbb0c2d2ab01c8dbd9b
Mickel Löfvenius <mickelsimon@gmail.com>
2020-12-02 12:52:57 +0100

    files added...

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,34 +1,37 @@
 import React, { useState } from "react";
 
+import "../css/AppNav.scss";
+
 import { Link } from "react-router-dom";
+
+//import ProfileCard from "./ProfileCard";
 
 function AppNav() {
   return (
     <div className="container">
-      <div className="ul-div">
-        <ul className="nav-ul">
-          <li>
-            <Link to="/">
-              <p>Home</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/map">
-              <p>Map</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <p>About</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profile">
-              <p>Profile</p>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <ul className="nav-ul">
+        <li>
+          <Link to="/">
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/map">
+            <p>Map</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <p>About</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/profile">
+            <p>Profile</p>
+          </Link>
+        </li>
+        <div>{/*<ProfileCard />*/}</div>
+      </ul>
     </div>
   );
 }
