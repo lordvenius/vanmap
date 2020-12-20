6eeb03c1563b5f6ff4f48fe33b632ba64c54d80c
Mickel Löfvenius <mickelsimon@gmail.com>
2020-12-20 14:33:41 +0100

    test commit

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -9,49 +9,46 @@ const Item = require("../../models/Item");
 // @desc Get All Items
 // @access Public
 router.get("/", (req, res) => {
-    Item.find()
-        .sort({ date: -1 })
-        .then(items => res.send(items))
+  Item.find()
+    .sort({ date: -1 })
+    .then((items) => res.send(items));
 });
 
 // @route POST api/items
-// @desc Create A item 
+// @desc Create A item
 // @access Public
 router.post("/", (req, res) => {
-    const newItem = new Item({
-        title: req.body.title,
-        description: req.body.description,
-        lat: req.body.lat,
-        lng: req.body.lng
+  const newItem = new Item({
+    title: req.body.title,
+    description: req.body.description,
+    lat: req.body.lat,
+    lng: req.body.lng,
+  });
 
-    });
-
-    newItem.save().then(item => res.json(item));
+  newItem.save().then((item) => res.json(item));
 });
 
 // @route PATCH
 // @desc Update A Item
 // @access public
 router.put("/:id", (req, res) => {
+  const updatedItem = new Item({
+    title: req.body.title,
+    description: req.body.description,
+    lat: req.body.lat,
+    lng: req.body.lng,
+  });
 
-    const updatedItem = new Item({
-        title: req.body.title,
-        description: req.body.description,
-        lat: req.body.lat,
-        lng: req.body.lng
-
-    });
-
-    updatedItem.save().then(item => res.json(item));
+  updatedItem.save().then((item) => res.json(item));
 });
 
 // @route DELETE api/items/:id
-// @desc Delete A Item 
+// @desc Delete A Item
 // @access Public
 router.delete("/:id", (req, res) => {
-    Item.findById(req.params.id)
-        .then(item => item.remove().then(() => res.json({ success: true })))
-        .catch(err => res.status(404).json({ success: false }))
+  Item.findById(req.params.id)
+    .then((item) => item.remove().then(() => res.json({ success: true })))
+    .catch((err) => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
