c900f473b832780072c8a204713533a8022150c8
Mickel Löfvenius <mickelsimon@gmail.com>
2020-12-21 19:32:40 +0100

    edited store because it only worked in chrome

--- src/store.js
+++ src/store.js
@@ -6,9 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
+
+export default store;
+
+/*
 const store = createStore(rootReducer, initialState, compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
-
-export default store;
+*/
